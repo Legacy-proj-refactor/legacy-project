b6e00e25f61b3c9b96d88d74ad2befe55f5788f5
louay <louaysakli65@gmail.com>
2023-12-28 11:12:01 +0100

    'initial updated'

---
+++ legacy/app/test/page.tsx
@@ -0,0 +1,220 @@
+
+export default function test(){
+   
+return(
+  <div className="abou">
+      
+   
+  <div className="our-story-paren">
+    <div className="our-stor">Our Story</div>
+    <div className="launced-in-2015-exclusive-is-paren">
+      <div className="launced-in-201">{`Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. `}</div>
+      <div className="exclusive-has-mor">
+        Exclusive has more than 1 Million products to offer, growing at a
+        very fast. Exclusive offers a diverse assotment in categories
+        ranging from consumer.
+      </div>
+    </div>
+  </div>
+  <div className="side-imag">
+    <img
+      className="portrait-two-african-females-h-ico"
+      alt=""
+      src="/portraittwoafricanfemalesholdingshoppingbagswhilereactingsomethingtheirsmartphone-1@2x.png"
+    />
+  </div>
+  <div className="full-service">
+    <div className="services-paren">
+      <img className="services-ico" alt="" src="/services@2x.png" />
+      <div className="free-and-fast-delivery-paren">
+        <div className="free-and-fas">FREE AND FAST DELIVERY</div>
+        <div className="free-delivery-fo">
+          Free delivery for all orders over $140
+        </div>
+      </div>
+    </div>
+    <div className="services-grou">
+      <img className="services-ico" alt="" src="/services@2x.png" />
+      <div className="customer-service-paren">
+        <div className="free-and-fas">24/7 CUSTOMER SERVICE</div>
+        <div className="friendly-247-custome">
+          Friendly 24/7 customer support
+        </div>
+      </div>
+    </div>
+    <div className="services-containe">
+      <img className="services-ico" alt="" src="/services@2x.png" />
+      <div className="free-and-fast-delivery-paren">
+        <div className="free-and-fas">MONEY BACK GUARANTEE</div>
+        <div className="friendly-247-custome">
+          We reurn money within 30 days
+        </div>
+      </div>
+    </div>
+    <div className="services-paren">
+      <img className="services-ico" alt="" src="/services@2x.png" />
+      <div className="free-and-fast-delivery-paren">
+        <div className="free-and-fas">MONEY BACK GUARANTEE</div>
+        <div className="friendly-247-custome">
+          We reurn money within 30 days
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="frame-grou">
+    <div className="instance-wrappe">
+      <div className="services-parent">
+        <img className="services-ico" alt="" src="/services@2x.png" />
+        <div className="free-and-fast-delivery-grou">
+          <b className="free-and-fast">{`10.5k `}</b>
+          <div className="free-delivery-for">Sallers active our site</div>
+        </div>
+      </div>
+    </div>
+    <div className="instance-containe">
+      <div className="services-parent">
+        <img className="services-ico" alt="" src="/services@2x.png" />
+        <div className="free-and-fast-delivery-paren">
+          <b className="free-and-fast">33k</b>
+          <div className="founder-chairma">Mopnthly Produduct Sale</div>
+        </div>
+      </div>
+    </div>
+    <div className="instance-wrappe">
+      <div className="services-parent">
+        <img className="services-ico" alt="" src="/services@2x.png" />
+        <div className="customer-service-grou">
+          <b className="free-and-fast">45.5k</b>
+          <div className="founder-chairma">
+            Customer active in our site
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="instance-wrappe">
+      <div className="services-parent">
+        <img className="services-ico" alt="" src="/services@2x.png" />
+        <div className="free-and-fast-delivery-grou">
+          <b className="free-and-fast">25k</b>
+          <div className="founder-chairma">
+            Anual gross sale in our site
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="frame-containe">
+    <div className="frame-parent1">
+      <div className="image-46-wrappe">
+        <img className="image-46-ico" alt="" src="/download.jfif" />
+      </div>
+      <div className="frame-parent2">
+        <div className="tom-cruise-paren">
+          <div className="tom-cruise">nourhen abidi</div>
+          <div className="founder-chairma">{`Founder & Chairman`}</div>
+        </div>
+        <div className="icon-twitter-paren">
+          <img className="dropdown-ico" alt="" src="/icontwitter@2x.png" />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconinstagram@2x.png"
+          />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconlinkedin@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+    <div className="frame-parent1">
+      <div className="image-46-wrappe">
+        <img className="image-51-ico" alt="" src="/louay.jpg" />
+      </div>
+      <div className="frame-parent2">
+        <div className="tom-cruise-paren">
+          <div className="tom-cruis">louay sakli</div>
+
+      
+
+          <div className="founder-chairma">Scrum master</div>
+        </div>
+        <div className="icon-twitter-paren">
+          <img className="dropdown-ico" alt="" src="/icontwitter@2x.png" />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconinstagram@2x.png"
+          />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconlinkedin@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+    <div className="frame-parent1">
+      <div className="image-46-wrappe">
+
+        <img className="image-51-ico" alt="" src="/salmen.jpg" />
+      </div>
+      <div className="frame-parent2">
+        <div className="tom-cruise-paren">
+          <div className="tom-cruis">aziz channoufi</div>
+          <div className="founder-chairma">Managing Director</div>
+        </div>
+        <div className="icon-twitter-paren">
+          <img className="dropdown-ico" alt="" src="/icontwitter@2x.png" />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconinstagram@2x.png"
+          />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconlinkedin@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+    <div className="frame-parent1">
+      <div className="image-46-wrappe">
+        <img className="image-47-ico" alt="" src="/gzgz.jpg" />
+      </div>
+      <div className="frame-parent2">
+        <div className="tom-cruise-paren">
+          <div className="tom-cruis">mohamed chokri</div>
+
+        
+
+          <div className="founder-chairma">Product Designer</div>
+        </div>
+        <div className="icon-twitter-paren">
+          <img className="dropdown-ico" alt="" src="/icontwitter@2x.png" />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconinstagram@2x.png"
+          />
+          <img
+            className="dropdown-ico"
+            alt=""
+            src="/iconlinkedin@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="ellipse-paren">
+    <div className="frame-chil" />
+    <div className="frame-chil" />
+    <img className="frame-inne" alt="" src="/group-1000005940@2x.png" />
+    <div className="frame-chil" />
+    <div className="frame-chil" />
+  </div>
+      </div>
+)
+}
